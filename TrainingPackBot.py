b5fc4c5e4a911ccf7fafa042747730c287a62cc4
PoldervaartS <shane.poldervaart@gmail.com>
2021-07-03 00:49:56 -0500

    Added dynamic environment figuring out. Cleaned up packs command to be more functional and cleaner

--- TrainingPackBot.py
+++ TrainingPackBot.py
@@ -8,13 +8,14 @@ import sys
 from HelperFunctions import *
 
 load_dotenv()
+isProd = getenv("BOTTOKENDEV") == None
 BASE_URL = 'https://ballchasingcoach.com/api/v1/'
 headers = {
     'Authorization': 'Bearer ' + str(getenv("APIBEARER")),
     'Content-Type': 'application/json',
     'Accept': 'application/json',
 }
-bot = commands.Bot(command_prefix="!", description="The description")
+bot = commands.Bot(command_prefix="!" if isProd else "~", description="Custom bot to find training packs and run the Wayprotein discord made by Daelisk")
 
 @bot.event
 async def  on_ready():
@@ -25,37 +26,33 @@ async def ping(ctx):
     await ctx.send('**pong**')
 
 @bot.command()
-async def packs(ctx, *args):
-    isVerbose = False
-    if 'verbose' in args:
-        args = [val for val in args if val != 'verbose']
-        isVerbose = True
-    params = {'page': 1}
+async def allpacks(ctx, description="Get The Spreadsheet"):
+    await ctx.send('https://docs.google.com/spreadsheets/d/1riHFd8KBBO9IqmbUbKPzgSDVpKOQXcb2UYaUUwFDs6M/edit#gid=1648371681')
 
-    # not sure how this works with the query, ask Ray
+@bot.command()
+async def packs(ctx, *args, help='Usage: `!packs`\t`!packs double tap creator=wayprotein sort=difficulty order=asc`', description="Get Training Packs"):
+    # flags map on possible args/options. https://ballchasingcoach.com/docs#apiv1training-packs gives list
+    flags = { 'verbose':False, 'sort':'', 'page':1, 'order':'desc', 'creator':''}
+    oldArgsLen = len(args)
+    args = [val for val in args if val != 'verbose']
+    flags['verbose'] = oldArgsLen > len(args)
+    getFlagsFromArgs(flags, args)
+    errorText = ""
+    if flags['order'] != 'desc' and flags['order'] != 'asc':
+        errorText = "Order arguement must be 'asc' or 'desc' "
+    if flags['sort'] != "" and flags['sort'] != 'difficulty':
+        errorText = errorText + "Sort arguement must be 'difficulty' "
+    if len(errorText) > 0:
+        await ctx.send(errorText)
+        return
+    params = {'page': flags['page'], 'sort':flags['sort'], 'order':flags['order'], 'creator':flags['creator'] }
     if len(args) > 0:
-        params['query'] = ",".join(args)
+        params['query'] = " ".join(args)
     response = requests.request('GET', BASE_URL + 'training-packs', headers=headers, params=params)
     data = response.json()['data']
-    formattedOutput = discord.Embed(title="Found Training Packs", color=discord.Color.blue())
-    if isVerbose:
-        [formattedOutput.add_field(name="{}".format(entry['creator']), value="{}\n{}\n{}\n{}\nNotes: {}\n{}".format(
-            entry['name'],
-            entry['code'], 
-            getEmojiForDifficulty(entry['difficulty']), 
-            entry['platform'],
-            entry['notes'],
-            entry['videoUrl'])) 
-            for index, entry in enumerate(data) if index < 6]
-        formattedOutput.set_footer(text='This query returns a maximum of 6 results, refine the search of check #browse-packs for more')
-    else:
-        [formattedOutput.add_field(name="{}".format(entry['creator']), value="{}\n{}\n{}".format(
-            entry['name'],
-            entry['code'], 
-            getEmojiForDifficulty(entry['difficulty']))) 
-            for index, entry in enumerate(data) if index < 12]
-        formattedOutput.set_footer(text='This query returns a maximum of 12 results, refine the search of check #browse-packs for more')
+
+    formattedOutput = getFormattedPacksOutput(data, flags['verbose'])
     await ctx.send(embed=formattedOutput)
     
 if __name__ == "__main__":
-    bot.run(getenv("BOTTOKEN"))
+    bot.run(getenv("BOTTOKEN") if isProd else getenv("BOTTOKENDEV"))
