3c196a731f5e61c1c0c145be17ecdbd4eb72780e
PoldervaartS <shane.poldervaart@gmail.com>
2021-07-17 17:21:16 -0500

    Added tags function Added yapf formatting

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -1,49 +1,73 @@
 import discord
 
+
 def getFlagsFromArgs(flags, args):
-    for i in range(len(args) -1, -1, -1):
+    for i in range(len(args) - 1, -1, -1):
         arg = args[i]
         values = arg.split("=")
         if values[0] in flags:
             flags[values[0]] = values[1]
             args.remove(arg)
 
+
 def getFormattedPacksOutput(data, isVerbose):
     formattedOutput = discord.Embed(title="Found Training Packs", color=discord.Color.blue())
 
-    if(len(data) == 0):
+    if (len(data) == 0):
         formattedOutput.add_field(name="No Packs Found", value="Try using only keywords or flags - !help packs")
         return formattedOutput
 
     if isVerbose:
-        [formattedOutput.add_field(name="{}".format(entry['creator']), value="{}\n{}\n{}\n{}\nNotes: {}\n{}".format(
-            entry['name'],
-            entry['code'], 
-            getEmojiForDifficulty(entry['difficulty']), 
-            entry['platform'],
-            entry['notes'],
-            entry['videoUrl'])) 
-            for index, entry in enumerate(data) if index < 6]
-        formattedOutput.set_footer(text='This query returns a maximum of 6 results, refine the search or check #browse-packs for more')
+        for index, entry in enumerate(data):
+            if index >= 6:
+                break
+            nameString = entry['creator']
+            valueString = "{}\n{}\n{}\n{}\nNotes: {}\n{}".format(entry['name'],
+                                                                 entry['code'],
+                                                                 getEmojiForDifficulty(entry['difficulty']),
+                                                                 entry['platform'],
+                                                                 entry['notes'],
+                                                                 entry['videoUrl'])
+            formattedOutput.add_field(name=nameString, value=valueString)
+        formattedOutput.set_footer(
+            text='This query returns a maximum of 6 results, refine the search or check #browse-packs for more')
     else:
-        [formattedOutput.add_field(name="{}".format(entry['creator']), value="{}\n{}\n{}".format(
-            entry['name'],
-            entry['code'], 
-            getEmojiForDifficulty(entry['difficulty']))) 
-            for index, entry in enumerate(data) if index < 12]
-        formattedOutput.set_footer(text='This query returns a maximum of 12 results, refine the search or check #browse-packs for more')
+        for index, entry in enumerate(data):
+            if index >= 12:
+                break
+            nameString = entry['creator']
+            valueString = "{}\n{}\n{}".format(entry['name'], entry['code'], getEmojiForDifficulty(entry['difficulty']))
+            formattedOutput.add_field(name=nameString, value=valueString)
+        formattedOutput.set_footer(
+            text='This query returns a maximum of 12 results, refine the search or check #browse-packs for more')
     return formattedOutput
 
+
+def getFormattedTagsOutput(data):
+    formattedOutput = discord.Embed(title="Here is a list of keywords you should search by", color=discord.Color.blue())
+    if len(data) == 0:
+        formattedOutput.add_field(name="No Tags Found",
+                                  value="Please Contact Bot and API Administrators (Daelisk#0001 & onlyray#9285")
+        return formattedOutput
+
+    tagsString = "".join(
+        ['**{}** : {}\n'.format(tag["name"], tag['counts']['packs']) for index, tag in enumerate(data)])
+    formattedOutput.add_field(name="Tag : Count", value=tagsString)
+    return formattedOutput
+
+
 def getEmojiForDifficulty(difficulty) -> str:
-    emojis = { 'Supersonic Legend': '<:Supersonic_Legend:849650907364982824>',
-    'Grand Champion':'<:Grand_champion3:849650869129576479>',
-    'Champion': '<:Champion3:849650848308658206>',
-    'Diamond': '<:Diamond3:849650835620495360>',
-    'Platinum': '<:Platinum3:849650882089189386>',
-    'Gold': '<:Gold3:849650858505535548>',
-    'Silver': '<:Silver3:849650894182023228>',
-    'Bronze': '<:Bronze3:849650820692705300>',
-    'Viewer': '<:Viewer:850202932544012309>' }
+    emojis = {
+        'Supersonic Legend': '<:Supersonic_Legend:849650907364982824>',
+        'Grand Champion': '<:Grand_champion3:849650869129576479>',
+        'Champion': '<:Champion3:849650848308658206>',
+        'Diamond': '<:Diamond3:849650835620495360>',
+        'Platinum': '<:Platinum3:849650882089189386>',
+        'Gold': '<:Gold3:849650858505535548>',
+        'Silver': '<:Silver3:849650894182023228>',
+        'Bronze': '<:Bronze3:849650820692705300>',
+        'Viewer': '<:Viewer:850202932544012309>'
+    }
 
     if difficulty == "":
         return emojis['Viewer']
--- TrainingPackBot.py
+++ TrainingPackBot.py
@@ -9,26 +9,68 @@ from HelperFunctions import *
 
 load_dotenv()
 isProd = getenv("BOTTOKENDEV") == None
-bot = commands.Bot(command_prefix="!" if isProd else "~", description="Custom bot to find training packs and run the Wayprotein discord made by Daelisk")
+bot = commands.Bot(command_prefix="!" if isProd else "~",
+                   description="Custom bot to find training packs and run the Wayprotein discord made by Daelisk")
 BASE_URL = ''
 headers = {}
 
+
 @bot.event
-async def  on_ready():
+async def on_ready():
     print("Ready !")
+
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('**pong**')
 
-@bot.command()
-async def allpacks(ctx, description="Get The Spreadsheet"):
-    await ctx.send('https://docs.google.com/spreadsheets/d/1riHFd8KBBO9IqmbUbKPzgSDVpKOQXcb2UYaUUwFDs6M/edit#gid=1648371681')
 
 @bot.command()
-async def packs(ctx, *args, help='Usage: `!packs`\t`!packs double tap creator=wayprotein sort=difficulty order=asc`', description="Get Training Packs"):
+async def allpacks(ctx, description="Get The Spreadsheet"):
+    await ctx.send(
+        'https://docs.google.com/spreadsheets/d/1riHFd8KBBO9IqmbUbKPzgSDVpKOQXcb2UYaUUwFDs6M/edit#gid=1648371681')
+
+
+@bot.command()
+async def tags(ctx, *args, descritption="Get the list of tags"):
+    flags = {'sort': '', 'page': 1, 'order': 'desc'}
+    getFlagsFromArgs(flags, args)
+    errorText = ""
+    if flags['order'] != 'desc' and flags['order'] != 'asc':
+        errorText = "Order arguement must be 'asc' or 'desc' "
+    if flags['sort'] != "" and flags['sort'] != 'packs':
+        errorText = errorText + "Sort arguement must be 'packs' "
+    if len(errorText) > 0:
+        await ctx.send(errorText)
+        return
+
+    response = requests.request('GET', BASE_URL + 'tags', headers=headers, params=flags)
+    data = response.json()['data']
+    formattedOutput = getFormattedTagsOutput(data)
+    await ctx.send(embed=formattedOutput)
+
+
+# @bot.command()
+# async def creators(ctx, descritption="Get the list of creators"):
+#     response = requests.request('GET', BASE_URL + 'creators', headers=headers)
+#     flags = {'sort': '', 'page': 1, 'order': 'desc'}
+#     getFlagsFromArgs(flags, args)
+#     errorText = ""
+#     if flags['order'] != 'desc' and flags['order'] != 'asc':
+#         errorText = "Order arguement must be 'asc' or 'desc' "
+#     if flags['sort'] != "" and flags['sort'] != 'packs':
+#         errorText = errorText + "Sort arguement must be 'packs' "
+#     formattedOutput = discord.Embed(title="Here is a list of creators you can search by", color=discord.Color.blue())
+#     formattedOutput.add_field(name="Creators", value=str(['{}\n'.format(creator) for creator in enumerate(response.json()['data'])]))
+
+
+@bot.command()
+async def packs(ctx,
+                *args,
+                help='Usage: `!packs`\t`!packs double tap creator=wayprotein sort=difficulty order=asc`',
+                description="Get Training Packs"):
     # flags map on possible args/options. https://ballchasingcoach.com/docs#apiv1training-packs gives list
-    flags = { 'verbose':False, 'sort':'', 'page':1, 'order':'desc', 'creator':''}
+    flags = {'verbose': False, 'sort': '', 'page': 1, 'order': 'desc', 'creator': ''}
     oldArgsLen = len(args)
     args = [val for val in args if val != 'verbose']
     flags['verbose'] = oldArgsLen > len(args)
@@ -41,7 +83,7 @@ async def packs(ctx, *args, help='Usage: `!packs`\t`!packs double tap creator=wa
     if len(errorText) > 0:
         await ctx.send(errorText)
         return
-    params = {'page': flags['page'], 'sort':flags['sort'], 'order':flags['order'], 'creator':flags['creator'] }
+    params = {'page': flags['page'], 'sort': flags['sort'], 'order': flags['order'], 'creator': flags['creator']}
     if len(args) > 0:
         params['query'] = " ".join(args)
     response = requests.request('GET', BASE_URL + 'training-packs', headers=headers, params=params)
@@ -49,12 +91,13 @@ async def packs(ctx, *args, help='Usage: `!packs`\t`!packs double tap creator=wa
 
     formattedOutput = getFormattedPacksOutput(data, flags['verbose'])
     await ctx.send(embed=formattedOutput)
-    
+
+
 if __name__ == "__main__":
     BASE_URL = 'https://ballchasingcoach.com/api/v1/'
     headers = {
-    'Authorization': 'Bearer ' + str(getenv("APIBEARER")),
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
+        'Authorization': 'Bearer ' + str(getenv("APIBEARER")),
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
     }
     bot.run(getenv("BOTTOKEN") if isProd else getenv("BOTTOKENDEV"))
