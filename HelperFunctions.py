e2348ba6a2ba25be83f936baa56fcadfbd2f2958
PoldervaartS <shane.poldervaart@gmail.com>
2021-07-18 11:43:33 -0500

    Added Creators command Added BotStrings class in hopes to reduse raw string usage I guess

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -1,4 +1,5 @@
 import discord
+from BotStrings import *
 
 
 def getFlagsFromArgs(flags, args):
@@ -14,7 +15,8 @@ def getFormattedPacksOutput(data, isVerbose):
     formattedOutput = discord.Embed(title="Found Training Packs", color=discord.Color.blue())
 
     if (len(data) == 0):
-        formattedOutput.add_field(name="No Packs Found", value="Try using only keywords or flags - !help packs")
+        formattedOutput.add_field(name=BotStrings.NO_RESULTS.format('packs'),
+                                  value="Try using only keywords, flags, or creators - !help packs")
         return formattedOutput
 
     if isVerbose:
@@ -29,8 +31,8 @@ def getFormattedPacksOutput(data, isVerbose):
                                                                  entry['notes'],
                                                                  entry['videoUrl'])
             formattedOutput.add_field(name=nameString, value=valueString)
-        formattedOutput.set_footer(
-            text='This query returns a maximum of 6 results, refine the search or check #browse-packs for more')
+        formattedOutput.set_footer(text=BotStrings.QUERY_MAXIMUM.format(6) +
+                                   ' Refine the search or check #browse-packs for more')
     else:
         for index, entry in enumerate(data):
             if index >= 12:
@@ -38,21 +40,37 @@ def getFormattedPacksOutput(data, isVerbose):
             nameString = entry['creator']
             valueString = "{}\n{}\n{}".format(entry['name'], entry['code'], getEmojiForDifficulty(entry['difficulty']))
             formattedOutput.add_field(name=nameString, value=valueString)
-        formattedOutput.set_footer(
-            text='This query returns a maximum of 12 results, refine the search or check #browse-packs for more')
+        formattedOutput.set_footer(text=BotStrings.QUERY_MAXIMUM.format(12) +
+                                   ' Refine the search or check #browse-packs for more')
     return formattedOutput
 
 
 def getFormattedTagsOutput(data):
-    formattedOutput = discord.Embed(title="Here is a list of keywords you should search by", color=discord.Color.blue())
+    formattedOutput = discord.Embed(title=BotStrings.LIST_OF_SEARCHABLE.format('tags'), color=discord.Color.blue())
     if len(data) == 0:
-        formattedOutput.add_field(name="No Tags Found",
+        formattedOutput.add_field(name=BotStrings.NO_RESULTS.format('tags'),
                                   value="Please Contact Bot and API Administrators (Daelisk#0001 & onlyray#9285")
         return formattedOutput
 
     tagsString = "".join(
         ['**{}** : {}\n'.format(tag["name"], tag['counts']['packs']) for index, tag in enumerate(data)])
     formattedOutput.add_field(name="Tag : Count", value=tagsString)
+    return formattedOutput
+
+
+def getFormattedCreatorsOutput(data):
+    formattedOutput = discord.Embed(title=BotStrings.LIST_OF_SEARCHABLE.format('creators'), color=discord.Color.blue())
+
+    if (len(data) == 0):
+        formattedOutput.add_field(name=BotStrings.NO_RESULTS.format('creators'),
+                                  value="Please Contact Bot and API Administrators (Daelisk#0001 & onlyray#9285)")
+        return formattedOutput
+
+    creatorsString = "".join(
+        ["**{}** : {}\n".format(creator['name'], creator['counts']['packs']) for index, creator in enumerate(data)])
+    formattedOutput.add_field(name="Creator : Packs Count", value=creatorsString)
+    formattedOutput.set_footer(text=BotStrings.QUERY_MAXIMUM.format(15) +
+                               ' Change page number for more results `page=2`')
     return formattedOutput
 
 
